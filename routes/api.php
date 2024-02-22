cfad2aa49a54f1bdf4d5aa498527d4d4c363b402
muhali16 <muhammadali55214@gmail.com>
2024-02-22 20:32:37 +0700

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthGoogleController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get("/auth/google/request", [AuthGoogleController::class, "googleAuth"]);
+// Route::get("/auth/google", [AuthGoogleController::class, "redirectToGoogle"]);
+Route::get("/auth/google/callback", [AuthGoogleController::class, "googleAuthCallback"]);
